f33d59bf1ebaec71b9bd53be23491f2d602a4b0e
David Ricardo Ramirez <kyra@Davids-MacBook-Pro.local>
2014-05-13 09:51:22 -0500

    added prueba class

---
+++ DRprueba.h
@@ -0,0 +1,13 @@
+//
+//  DRprueba.h
+//  Prueba Git
+//
+//  Created by David Ricardo Ramirez on 5/13/14.
+//  Copyright (c) 2014 David Ricardo Ramirez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DRprueba : NSObject
+
+@end
---
+++ DRprueba.m
@@ -0,0 +1,13 @@
+//
+//  DRprueba.m
+//  Prueba Git
+//
+//  Created by David Ricardo Ramirez on 5/13/14.
+//  Copyright (c) 2014 David Ricardo Ramirez. All rights reserved.
+//
+
+#import "DRprueba.h"
+
+@implementation DRprueba
+
+@end
